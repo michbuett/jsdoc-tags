5898ab4fd28cf2d5c65f17ce583efaea46e61897
Michael Buettner <michael.buettner@seitenbau.com>
2013-04-12 15:07:09 +0200

    Allow to change output folder

--- bin/create-tags.sh
+++ bin/create-tags.sh
@@ -1,27 +1,29 @@
 #! /bin/bash
 
+SOURCE=$1
+OUT_DIR=$2
+
+if [ ! $OUT_DIR ]; then
+    OUT_DIR="$PWD"
+fi
+
 BASE_DIR=$( unset CDPATH && cd "$(dirname $0)/.." && pwd)
 LIB_DIR="$BASE_DIR/support/jsdoc-toolkit"
-OUT_DIR="$BASE_DIR/out"
 JAR_DIR="$LIB_DIR/jsrun.jar"
 APP_DIR="$LIB_DIR/app/run.js"
 TPL_DIR="$BASE_DIR/templates/tags"
 
-SOURCE=$1
-
-# clear output folder
-if [ -d $OUT_DIR ]; then
-    rm -rf $OUT_DIR
-fi
-mkdir -p $OUT_DIR
+echo "### JsDoc Tag Generator ###"
+echo "[INFO] Tagfile saved to $OUT_DIR"
+echo "[INFO] Processing $SOURCE..."
 
 # execte jsdoc
 cmd="java -jar $JAR_DIR $APP_DIR \
     --template=$TPL_DIR \
     --directory=$OUT_DIR \
     --recurse \
-    --allfunctions \
     --private \
+    --quiet \
     $SOURCE"
-echo "RUNNING: $cmd"
+#echo "RUNNING: $cmd"
 $cmd
